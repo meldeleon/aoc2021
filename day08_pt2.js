8f2764161c7f9b4de6acb7b3e089f28fc6899d9a
meldeleon <melissadeleon@gmail.com>
2022-01-08 17:05:32 -0800

    starting to fix day8 pt 2

--- day08_pt2.js
+++ day08_pt2.js
@@ -1,7 +1,3 @@
-const { createSecretKey } = require("crypto")
-const { abort } = require("process")
-const { unzipSync } = require("zlib")
-
 let data = require("fs")
   .readFileSync("day08_input.txt")
   .toString()
@@ -12,9 +8,9 @@ data.forEach((row) => {
   let [input, output] = row.split(/\s\|\s/)
   //console.log({ input, output }) // ure_funny has blown my mind.
   let inputArray = input.split(/\s/)
+  let outputArray = output.split(/\s/)
   //console.log({ inputArray })
   let digitAssignments = {}
-  let segmentAssignments = {}
   //assign the stuff we do know
   inputArray.forEach((digit, index) => {
     switch (digit.length) {
@@ -36,20 +32,33 @@ data.forEach((row) => {
   })
   console.log(digitAssignments)
   inputArray.forEach((digit) => {
-    switch (digit.length) {
-      case 6:
-        let oneCountA = createCount(1, digitAssignments, digit)
-        let fourCountA = createCount(4, digitAssignments, digit)
-        let sevenCountA = createCount(7, digitAssignments, digit)
-        let sumA = oneCountA + fourCountA + sevenCountA
-        if (sumA === 8) {
-          digitAssignments[0] = digit.split("")
-        } else if (sumA === 6) {
-          digitAssignments[6] = digit.split("")
-        } else if (sumA === 9) {
-          digitAssignments[9] = digit.split("")
-        }
-        break
+    if (digit.length === 6) {
+      let oneCountA = createCount(1, digitAssignments, digit)
+      let fourCountA = createCount(4, digitAssignments, digit)
+      let sevenCountA = createCount(7, digitAssignments, digit)
+      let sumA = oneCountA + fourCountA + sevenCountA
+      if (sumA === 8) {
+        digitAssignments[0] = digit.split("")
+      } else if (sumA === 6) {
+        digitAssignments[6] = digit.split("")
+      } else if (sumA === 9) {
+        digitAssignments[9] = digit.split("")
+      }
+    }
+  })
+  inputArray.forEach((digit) => {
+    if (digit.length === 5) {
+      let sixCountB = createCount(6, digitAssignments, digit)
+      let nineCountB = createCount(9, digitAssignments, digit)
+      let sumB = sixCountB + nineCountB
+      console.log(sumB, digit)
+      if (sumB === 8) {
+        digitAssignments[2] = digit.split("")
+      } else if (sumB === 9) {
+        digitAssignments[3] = digit.split("")
+      } else if (sumB === 5) {
+        digitAssignments[5] = digit.split("")
+      }
     }
   })
   console.log(digitAssignments)
@@ -64,3 +73,11 @@ function createCount(comparator, digitAssignments, digit) {
   })
   return count
 }
+
+function parseOutput(digitAssignments, outputArray) {
+  outputArray.forEach((display) => {
+    display.forEach
+  })
+}
+
+function sumSolution() {}
